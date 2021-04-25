4b69b7cd000cbf5a84a1d8e44cf168c5467947ae
Marcelo Gouveia <mgouveiadeassis@gmail.com>
2021-04-25 16:50:33 -0300

    resetando CSS e estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 import './App.css';
 
 function App() {
   return (
     <div>
-      App React
+      <BrowserRouter>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
