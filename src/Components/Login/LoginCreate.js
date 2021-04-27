c5313bec673600aa6df5a11d7f42ee95edde9e0f
Marcelo Gouveia <mgouveiadeassis@gmail.com>
2021-04-26 21:47:49 -0300

    feat: create user

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import useForm from '../../Hooks/useForm';
+import Button from '../Forms/Button';
+import Input from '../Forms/Input';
+import {USER_POST} from '../../api';
+import { UserContext } from '../../UserContext';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
 
 const LoginCreate = () => {
+    const username = useForm();
+    const email = useForm('email');
+    const password = useForm();
+
+    const { userLogin } = React.useContext(UserContext);
+    const { request, loading, error } = useFetch();
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const {url, options} = USER_POST({
+            username: username.value,
+            email: email.value,
+            password: password.value,
+        });
+        const { response } = await request(url, options);
+        if(response.ok) userLogin(username.value, password.value);
+    }
+
     return (
-        <div>
-            Login Create
-        </div>
+        <section className="animeLeft">
+            <h1 className="title">Cadastre-se</h1>
+            <form onSubmit={handleSubmit}>
+                <Input label="Usuário" type="text" name="username" {...username}/>
+                <Input label="Email" type="email" name="email" {...email}/>
+                <Input label="Senha" type="password" name="password" {...password}/>
+                { loading ? <Button disabled>Cadastrando...</Button> : <Button>Cadastrar</Button> }
+                <Error error={error}/>
+            </form>
+        </section>
     )
 }
 
