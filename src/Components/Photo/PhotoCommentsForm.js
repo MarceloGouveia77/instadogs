def2f6d4b26860d74af94a0b51f226c1ed436d5d
Marcelo Gouveia <mgouveiadeassis@gmail.com>
2021-05-01 21:39:24 -0300

    feat: comentários

---
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { COMMENT_POST } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+import {ReactComponent as Enviar} from '../Assets/enviar.svg';
+import Error from '../Helper/Error';
+
+const PhotoCommentsForm = ({ id, setComments }) => {
+    const [comment, setComment] = React.useState('');
+    const { request, error } = useFetch();
+  
+    async function handleSubmit(event) {
+      event.preventDefault();
+      const { url, options } = COMMENT_POST(id, { comment });
+      const { response, json } = await request(url, options);
+      console.log(json);
+      if (response.ok) {
+        setComment('');
+        setComments((comments) => [...comments, json]);
+      }
+    }
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <textarea
+          id="comment"
+          name="comment"
+          placeholder="Comente..."
+          value={comment}
+          onChange={({ target }) => setComment(target.value)}
+        />
+        <button>
+          <Enviar />
+        </button>
+        <Error error={error} />
+      </form>
+    );
+  };
+  
+  export default PhotoCommentsForm;
+  
