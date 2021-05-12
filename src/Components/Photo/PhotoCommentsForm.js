f9f920831b1681595fface37b9ded0920c094385
Marcelo Gouveia <mgouveiadeassis@gmail.com>
2021-05-12 20:35:29 -0300

    style: comment section

--- src/Components/Photo/PhotoCommentsForm.js
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -3,6 +3,7 @@ import { COMMENT_POST } from '../../api';
 import useFetch from '../../Hooks/useFetch';
 import {ReactComponent as Enviar} from '../Assets/enviar.svg';
 import Error from '../Helper/Error';
+import styles from './PhotoCommentsForm.module.css';
 
 const PhotoCommentsForm = ({ id, setComments }) => {
     const [comment, setComment] = React.useState('');
@@ -20,15 +21,15 @@ const PhotoCommentsForm = ({ id, setComments }) => {
     }
   
     return (
-      <form onSubmit={handleSubmit}>
-        <textarea
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <textarea className={styles.textarea}
           id="comment"
           name="comment"
           placeholder="Comente..."
           value={comment}
           onChange={({ target }) => setComment(target.value)}
         />
-        <button>
+        <button className={styles.button}>
           <Enviar />
         </button>
         <Error error={error} />
