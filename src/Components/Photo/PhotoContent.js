6363fc954152a97a5e66ae454e452ad4a82a04e8
Marcelo Gouveia <mgouveiadeassis@gmail.com>
2021-05-12 20:59:19 -0300

    feat: deletar fotos

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './PhotoContent.module.css';
 import PhotoComments from './PhotoComments';
+import { UserContext } from '../../UserContext';
+import PhotoDelete from './PhotoDelete';
 
 const PhotoContent = ({ data }) => {
+    const user = React.useContext(UserContext);
     const {photo, comments} = data;
 
     return (
@@ -13,10 +16,14 @@ const PhotoContent = ({ data }) => {
             </div>
             <div className={styles.details}>
                 <div>
-                    <p className={styles.author}>
-                        <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
-                        <span className={styles.visualizacoes}>{photo.acessos}</span>
-                    </p>
+                <p className={styles.author}>
+                    {user.data && user.data.username === photo.author ? (
+                    <PhotoDelete id={photo.id} />
+                    ) : (
+                    <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+                    )}
+                    <span className={styles.visualizacoes}>{photo.acessos}</span>
+                </p>
                     <h1 className="title"><Link to={`/photo/${photo.id}`}>{photo.title}</Link></h1>
                     <ul className={styles.attributes}>
                         <li>{photo.peso} kg</li>
