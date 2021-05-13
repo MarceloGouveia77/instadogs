baa5c7a433ac8eafaecd3b5f66152c78c6f0265c
Marcelo Gouveia <mgouveiadeassis@gmail.com>
2021-05-12 22:46:36 -0300

    feat: pagina da foto

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import Photo from './Components/Photo/Photo';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="login/*" element={<Login />} />
               <ProtectedRoute path="conta/*" element={<User />} />
+              <Route path="photo/:id" element={<Photo />} />
             </Routes>
           <Footer />
         </UserStorage>
